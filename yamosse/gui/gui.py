dc0501f1a9f4fc73d760f4a27786ce7c1321e737
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2025-07-13 17:54:07 -0600

    make the title font slightly larger

--- yamosse/gui/gui.py
+++ yamosse/gui/gui.py
@@ -724,7 +724,7 @@ def make_window(window, make_frame, *args, **kwargs):
   
   style = ttk.Style(window)
   style.configure('Debug.TFrame', background='Red', relief=tk.GROOVE)
-  style.configure('Title.TLabel', font=('Trebuchet MS', 18))
+  style.configure('Title.TLabel', font=('Trebuchet MS', 24))
   
   style.layout('Borderless.TNotebook', [])
   style.configure('Borderless.TNotebook > .TFrame', relief=tk.RAISED)
