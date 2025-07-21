ae71a7bb1dce834ed63bc091f175ff1bd05f4ed9
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2025-07-21 04:55:16 -0600

    save calibration back to options, also make Calibrate window slightly wider so adjusting the scales is easier

--- yamosse/gui/calibrate.py
+++ yamosse/gui/calibrate.py
@@ -6,7 +6,7 @@ from .. import gui
 TITLE = 'Calibrate'
 
 
-def make_footer(frame, window):
+def make_footer(frame, ok, cancel):
   frame.columnconfigure(2, weight=1)
   
   # TODO: undoable commands
@@ -20,17 +20,13 @@ def make_footer(frame, window):
   
   redo_button.grid(row=0, column=1, sticky=tk.W, padx=gui.PADX_QW)
   
-  ok_button = ttk.Button(frame, text='OK', underline=0,
-    command=lambda: gui.release_modal_window(window), default=tk.ACTIVE)
-  
+  ok_button = ttk.Button(frame, text='OK', underline=0, command=ok, default=tk.ACTIVE)
   ok_button.grid(row=0, column=3, sticky=tk.E, padx=gui.PADX_QW)
   
-  cancel_button = ttk.Button(frame, text='Cancel', underline=0,
-    command=lambda: gui.release_modal_window(window))
-  
+  cancel_button = ttk.Button(frame, text='Cancel', underline=0, command=cancel)
   cancel_button.grid(row=0, column=4, sticky=tk.E, padx=gui.PADX_QW)
   
-  gui.bind_buttons_window(window, ok_button=ok_button, cancel_button=cancel_button)
+  gui.bind_buttons_window(frame.winfo_toplevel(), ok_button=ok_button, cancel_button=cancel_button)
   
   for button in (ok_button, cancel_button):
     gui.enable_traversal_button(button)
@@ -38,7 +34,7 @@ def make_footer(frame, window):
 
 def make_calibrate(frame, variables, class_names):
   RESIZABLE = True
-  SIZE = (480, 500)
+  SIZE = (520, 500)
   
   window = frame.master
   parent = window.master
@@ -46,35 +42,46 @@ def make_calibrate(frame, variables, class_names):
   gui.customize_window(window, TITLE, resizable=RESIZABLE, size=SIZE,
     location=gui.location_center_window(parent, SIZE))
   
-  frame.rowconfigure(0, weight=1) # make classes frame vertically resizable
-  frame.columnconfigure(0, weight=1) # make classes frame horizontally resizable
+  frame.rowconfigure(0, weight=1) # make calibration frame vertically resizable
+  frame.columnconfigure(0, weight=1) # make calibration frame horizontally resizable
   
-  classes_frame = ttk.Frame(frame, relief=tk.SUNKEN, borderwidth=4)
-  classes_frame.grid(row=0, sticky=tk.NSEW)
+  calibration_frame = ttk.Frame(frame, relief=tk.SUNKEN, borderwidth=4)
+  calibration_frame.grid(row=0, sticky=tk.NSEW)
   
-  classes_text = gui.make_text(classes_frame, font=('TkDefaultFont', 24))[1][0]
-  gui.embed_text(classes_text)
+  calibration_text = gui.make_text(calibration_frame, font=('TkDefaultFont', 24))[1][0]
+  gui.embed_text(calibration_text)
   
   # put in 100% as defaults if the calibration is empty/too short
   calibration_variable = variables['calibration']
   calibration_variable += [100] * (len(class_names) - len(calibration_variable))
   
+  scales = []
+  
   for c in range(len(class_names)):
-    scale_frame = ttk.Frame(classes_text)
+    scale_frame = ttk.Frame(calibration_text)
     
     scale = gui.make_scale(scale_frame, name='%d. %s' % (c + 1, class_names[c]),
       to=200)[1]
     
     scale.set(calibration_variable[c])
+    scales.append(scale)
     
     scale_frame.columnconfigure(0, weight=2, uniform='class_column')
     scale_frame.columnconfigure(1, weight=1, uniform='class_column')
     
-    gui.embed_insert(classes_text, scale_frame)
+    gui.embed_insert(calibration_text, scale_frame)
   
   footer_frame = ttk.Frame(frame)
   footer_frame.grid(row=1, sticky=tk.EW, pady=gui.PADY_N)
   
-  make_footer(footer_frame, window)
+  def ok():
+    variables['calibration'] = [s.get() for s in scales]
+    gui.release_modal_window(window)
+  
+  make_footer(
+    footer_frame,
+    ok,
+    lambda: gui.release_modal_window(window)
+  )
   
   gui.set_modal_window(window)
