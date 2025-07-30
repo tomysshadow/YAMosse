0b5ed974577bdae037b60f8af2cd4c7790342799
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2025-07-30 15:09:23 -0600

    provide a link to the undoable widgets page

--- yamosse/gui/__init__.py
+++ yamosse/gui/__init__.py
@@ -995,6 +995,7 @@ def make_undoable(frame):
     finally:
       undoing = False
   
+  # https://wiki.tcl-lang.org/page/Undo+and+Redo+undoable+widgets
   undoings = [] # list of undoable things - each is a 2 part list 
           # first the arguments to undo the operation;
           # then the arguments to redo the operation.
