4f9794cec649071d5c6922921e6457f9b1d4e948
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2025-08-04 03:23:43 -0600

    make the output tests more flexible

--- tests/test_output.py
+++ tests/test_output.py
@@ -14,8 +14,6 @@ SUFFIX_JSON = '.json'
 
 class TestOutput:
   def setUp(self, mode='w+b'):
-    self.options = options.Options()
-    
     self.file = tempfile.NamedTemporaryFile(
       mode=mode,
       suffix=SUFFIX_TXT,
@@ -26,6 +24,11 @@ class TestOutput:
     file = self.file
     file.close()
     unlink(file.name)
+  
+  def _set_options(self, **kwargs):
+    o = options.Options()
+    o.set(kwargs, strict=False)
+    return o
   
   def _output_file(self, identification=0):
     file = self.file
@@ -43,70 +46,95 @@ class TestOutputText(TestOutput, unittest.TestCase):
   def test_output_options(self):
     o, f = self._output_file()
     
-    with o: o.options(self.options)
+    with o: o.options(self._set_options())
     
     self.assertEqual(f.readline(), '# Options\n')
     self.assertTrue(f.read().endswith('\n\n'))
   
-  def test_output_results_cs(self):
-    CLASS_TIMESTAMPS = {
-      0: {0: 75.0, 3: 50.0, 6: 25.0},
-      1: {0: 75.0, 3: 50.0, 6: 25.0},
-      2: {0: 75.0, 3: 50.0, 6: 25.0}
-    }
+  def _output_results_cs(self, class_timestamps, **kwargs):
+    options = self._set_options(output_options=False, **kwargs)
+    item_delimiter = options.item_delimiter
+    output_scores = options.output_scores
     
-    RESULTS = {
-      'File Name.wav': CLASS_TIMESTAMPS,
-      'File Name 2.wav': CLASS_TIMESTAMPS
+    results = {
+      'File Name.wav': class_timestamps,
+      'File Name 2.wav': class_timestamps
     }
     
     o, f = self._output_file(0)
     
-    with o: o.results(RESULTS)
+    with o:
+      o.options(options)
+      o.results(results)
     
     self.assertEqual(f.readline(), '# Results\n')
     self.assertEqual(f.readline(), '\n')
     
-    for file_name in RESULTS.keys():
+    for file_name in results.keys():
       self.assertEqual(f.readline(), ''.join((quote(file_name), '\n')))
       
-      for class_ in CLASS_TIMESTAMPS.keys():
+      for class_, timestamps in class_timestamps.items():
         self.assertEqual(f.readline(), ''.join(('\t', MODEL_YAMNET_CLASS_NAMES[class_], ':\n')))
-        self.assertEqual(f.readline(), ''.join(('\t\t', '0:00 0:03 0:06', '\n')))
+        
+        if output_scores:
+          timestamps = [f'{utils.hours_minutes(ts)} ({s:.0%})' for ts, s in timestamps.items()]
+        else:
+          timestamps = [utils.hours_minutes(ts) for ts in timestamps]
+        
+        self.assertEqual(f.readline(), ''.join(('\t\t', item_delimiter.join(timestamps), '\n')))
+      
+      self.assertEqual(f.readline(), '\n')
+    
+    self.assertEqual(f.readline(), '\n')
+  
+  def test_output_results_cs(self):
+    self._output_results_cs({
+      0: {0: 75.0, 3: 50.0, 6: 25.0},
+      1: {0: 75.0, 3: 50.0, 6: 25.0},
+      2: {0: 75.0, 3: 50.0, 6: 25.0}
+    })
+  
+  def _output_results_tr(self, timestamp_classes, **kwargs):
+    options = self._set_options(output_options=False, **kwargs)
+    item_delimiter = options.item_delimiter
+    output_scores = options.output_scores
+    
+    results = {
+      'File Name.wav': timestamp_classes,
+      'File Name 2.wav': timestamp_classes
+    }
+    
+    o, f = self._output_file(1)
+    
+    with o:
+      o.options(options)
+      o.results(results)
+    
+    self.assertEqual(f.readline(), '# Results\n')
+    self.assertEqual(f.readline(), '\n')
+    
+    for file_name in results.keys():
+      self.assertEqual(f.readline(), ''.join((quote(file_name), '\n')))
+      
+      for timestamp, classes in timestamp_classes.items():
+        if output_scores:
+          classes = [f'{MODEL_YAMNET_CLASS_NAMES[c]} ({s:.0%})' for c, s in classes.items()]
+        else:
+          classes = [MODEL_YAMNET_CLASS_NAMES[c] for c in classes.keys()]
+        
+        self.assertEqual(f.readline(), ''.join(('\t', utils.hours_minutes(timestamp), ': ',
+          item_delimiter.join(classes), '\n')))
       
       self.assertEqual(f.readline(), '\n')
     
     self.assertEqual(f.readline(), '\n')
   
   def test_output_results_tr(self):
-    TIMESTAMP_CLASSES = {
+    self._output_results_tr({
       0: {0: 75.0, 1: 50.0, 2: 25.0},
       3: {0: 75.0, 1: 50.0, 2: 25.0},
       6: {0: 75.0, 1: 50.0, 2: 25.0}
-    }
-    
-    RESULTS = {
-      'File Name.wav': TIMESTAMP_CLASSES,
-      'File Name 2.wav': TIMESTAMP_CLASSES
-    }
-    
-    o, f = self._output_file(1)
-    
-    with o: o.results(RESULTS)
-    
-    self.assertEqual(f.readline(), '# Results\n')
-    self.assertEqual(f.readline(), '\n')
-    
-    for file_name in RESULTS.keys():
-      self.assertEqual(f.readline(), ''.join((quote(file_name), '\n')))
-      
-      for timestamp, classes_scores in TIMESTAMP_CLASSES.items():
-        self.assertEqual(f.readline(), ''.join(('\t', utils.hours_minutes(timestamp), ': ',
-          ' '.join([MODEL_YAMNET_CLASS_NAMES[c] for c in classes_scores.keys()]), '\n')))
-      
-      self.assertEqual(f.readline(), '\n')
-    
-    self.assertEqual(f.readline(), '\n')
+    })
   
   def test_output_errors(self):
     ERRORS = {
