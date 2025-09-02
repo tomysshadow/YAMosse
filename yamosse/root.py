9cef0a0d4fed73ad3d7e27e4f3efddf6797bac4c
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2025-09-02 17:12:01 -0600

    don't find the root dir every time it's used

--- yamosse/root.py
+++ yamosse/root.py
@@ -1,7 +1,10 @@
 from os import path
 
-def root(dir_):
+_root_dir = path.dirname(path.realpath(__file__))
+
+
+def root(dir_=''):
   if path.isabs(dir_):
     return dir_
   
-  return path.join(path.dirname(path.realpath(__file__)), dir_)
+  return path.join(_root_dir, dir_)
