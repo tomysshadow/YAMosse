67ae1ddb9c4ded8dbca95229d167b2030b625bcf
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2025-09-03 04:27:09 -0600

    use argparse to exit the program

--- yamosse/__main__.py
+++ yamosse/__main__.py
@@ -1,4 +1,3 @@
-import sys
 import argparse
 
 import yamosse
@@ -7,55 +6,50 @@ import yamosse.worker as yamosse_worker
 import yamosse.subsystem as yamosse_subsystem
 
 
-def main():
-  print(yamosse.TITLE, end='\n\n')
-  
-  parser = argparse.ArgumentParser(
-    prog=yamosse.__name__,
-    description=yamosse.__doc__
-  )
-  
-  parser.add_argument('-rd', '--restore_defaults',
-    action='store_true', default=argparse.SUPPRESS)
-  
-  parser.add_argument('-r', '--record',
-    action='store_true', default=argparse.SUPPRESS)
-  
-  parser.add_argument('-ip', '--import-preset',
-    dest='import_preset_file_name', default=argparse.SUPPRESS)
-  
-  parser.add_argument('-ep', '--export-preset',
-    dest='export_preset_file_name', default=argparse.SUPPRESS)
-  
-  parser.add_argument('-y', '--yamscan',
-    dest='output_file_name', default=argparse.SUPPRESS)
-  
-  parser.add_argument('-o', '--option', nargs=2,
-    action='append', dest='options_attrs', metavar=('KEY', 'VALUE'), default=[])
-  
-  args = parser.parse_args()
-  options_attrs = dict(args.options_attrs)
-  
-  for key, value in options_attrs.items():
-    try:
-      options_attrs[key] = yamosse_options.json.loads(value)
-    except (yamosse_options.json.JSONDecodeError, UnicodeDecodeError) as ex:
-      parser.error(str(ex))
-  
-  if options_attrs:
-    args.options_attrs = options_attrs
-  else:
-    del args.options_attrs
-  
-  yamosse_worker.tfhub_cache()
-  
+print(yamosse.TITLE, end='\n\n')
+
+parser = argparse.ArgumentParser(
+  prog=yamosse.__name__,
+  description=yamosse.__doc__
+)
+
+parser.add_argument('-rd', '--restore_defaults',
+  action='store_true', default=argparse.SUPPRESS)
+
+parser.add_argument('-r', '--record',
+  action='store_true', default=argparse.SUPPRESS)
+
+parser.add_argument('-ip', '--import-preset',
+  dest='import_preset_file_name', default=argparse.SUPPRESS)
+
+parser.add_argument('-ep', '--export-preset',
+  dest='export_preset_file_name', default=argparse.SUPPRESS)
+
+parser.add_argument('-y', '--yamscan',
+  dest='output_file_name', default=argparse.SUPPRESS)
+
+parser.add_argument('-o', '--option', nargs=2,
+  action='append', dest='options_attrs', metavar=('KEY', 'VALUE'), default=[])
+
+args = parser.parse_args()
+options_attrs = dict(args.options_attrs)
+
+for key, value in options_attrs.items():
   try:
-    yamosse.yamosse(**vars(args))
-  except yamosse_subsystem.SubsystemError as ex:
-    print(ex)
-    return 1
-  
-  return 0
+    options_attrs[key] = yamosse_options.json.loads(value)
+  except (yamosse_options.json.JSONDecodeError, UnicodeDecodeError) as ex:
+    parser.error(str(ex))
 
+if options_attrs:
+  args.options_attrs = options_attrs
+else:
+  del args.options_attrs
 
-sys.exit(main())
+yamosse_worker.tfhub_cache()
+
+try:
+  yamosse.yamosse(**vars(args))
+except yamosse_subsystem.SubsystemError as ex:
+  parser.exit(status=1, message=str(ex))
+
+parser.exit()
