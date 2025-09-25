aaa34304bef6fdff37ed587848e5ef17a940920d
DESKTOP-0Q3GGGJ\Anthony <ellescuba@gmail.com>
2025-09-25 00:31:04 -0600

    minor tweak

--- yamosse/utils.py
+++ yamosse/utils.py
@@ -55,4 +55,4 @@ def dict_enumerate(d, *args, **kwargs):
 
 
 def batched(seq, size):
-  return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+  return (seq[pos:pos+size] for pos in range(0, len(seq), size))
